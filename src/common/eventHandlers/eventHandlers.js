3c55e7eece2659963c19fd9db1220e10160627f1
Phil Toms <phil.toms@hotmail.co.uk>
2013-11-28 14:49:05 +0000

    fix(highlightOnHover): allow for current status to become null

---
+++ src/common/eventHandlers/eventHandlers.js
@@ -0,0 +1,47 @@
+angular.module('eventHandlers',[])
+
+.directive('highlightOnHover', function(){
+  var currentFocus=$();
+  var rows;
+  return {
+    link:function(scope,elem,attr){
+      $(elem).on('mouseover','td', function(event){
+        rows = rows || $(elem).find('tbody > tr');
+        if (currentFocus) {
+          currentFocus.removeClass('highlight');
+        }
+        currentFocus=$(this).parent();
+
+        var index = $(this).prevAll().length;  
+        var columns = rows.find(':nth-child(' + (index + 1) + ')');
+        currentFocus = currentFocus.add(columns);
+        currentFocus.addClass('highlight');
+      });
+      scope.$on('gsa data updated', function(){
+        if (currentFocus){
+          currentFocus.removeClass('highlight');
+        }
+        currentFocus=null;
+      });
+    }
+  };
+})
+
+.directive('scrollbarOnHover', function(){
+  var width;
+  return {
+    transclude:true,
+    template:'<div id="container" ng-transclude></div>',
+    link:function(scope,elem,attr){
+      width = width || $(elem).width();
+      $(elem).on('mouseover', function(event){
+        if (event.clientX > 1295){
+          $(elem).width(width+16);
+        }
+        else {
+          $(elem).width(width);
+        }
+      });
+    }
+  };
+});
